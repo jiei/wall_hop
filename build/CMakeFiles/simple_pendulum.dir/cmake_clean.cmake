2e6764dae23110089e1fe9d56b94d42993586086
jiei <jack0325suzu@docomo.ne.jp>
2018-11-17 16:11:27 +0900

    initial commit

---
+++ build/CMakeFiles/simple_pendulum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_pendulum.dir/simple_pendulum.cpp.o"
+  "simple_pendulum.pdb"
+  "simple_pendulum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_pendulum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
